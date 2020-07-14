fd355742bee4f921c2e1613c2dcc98a7f7f89bec
ouyangcan <619491800@qq.com>
2020-07-14 22:10:54 +0800

    增加spring-cloud-config子项目；ribbon-api通过配置中心获取user-api的服务名

--- ribbon-api/src/main/java/com/francis/ribbon/service/impl/RibbonServiceImpl.java
+++ ribbon-api/src/main/java/com/francis/ribbon/service/impl/RibbonServiceImpl.java
@@ -8,6 +8,7 @@ import com.francis.ribbon.service.RibbonService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -24,11 +25,14 @@ public class RibbonServiceImpl implements RibbonService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${api.user.serviceName}")
+    private String userApiServiceName;
+
     @HystrixCommand(fallbackMethod = "queryUserByIdFallback", ignoreExceptions = NullPointerException.class)
     @Override
     public String queryUserById(String userId) {
-        final ResponseEntity<ResponseJson> entity
-                = restTemplate.getForEntity("http://USER-API/user-api/user/info?userId=" + userId, ResponseJson.class);
+        final ResponseEntity<ResponseJson> entity = restTemplate.getForEntity(
+                "http://" + userApiServiceName + "/user-api/user/info?userId=" + userId, ResponseJson.class);
         if (entity.getStatusCode().equals(HttpStatus.OK)) {
             final User user =  JSONObject.toJavaObject((JSON) JSONObject.toJSON(entity.getBody().getData()), User.class);
             return "User name is " + user.getUserName() + ", age is " + user.getAge() + ", phone is " + user.getPhone();
---
+++ spring-cloud-config/src/main/java/com/francis/config/SpringCloudConfigApplication.java
@@ -0,0 +1,17 @@
+package com.francis.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigApplication.class, args);
+    }
+
+}
