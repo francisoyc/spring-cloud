94b0fab042c61af361ef585d7310bc2953554595
ouyangcan <619491800@qq.com>
2020-07-19 22:34:38 +0800

    增加gateway工程，ribbon-api增加获取实例信息接口

--- ribbon-api/src/main/java/com/francis/ribbon/controller/RibbonController.java
+++ ribbon-api/src/main/java/com/francis/ribbon/controller/RibbonController.java
@@ -2,9 +2,13 @@ package com.francis.ribbon.controller;
 
 import com.francis.ribbon.service.RibbonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author: francis
@@ -17,8 +21,17 @@ public class RibbonController {
     @Autowired
     private RibbonService service;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping(value = "/user/info/{userId}", produces = "application/json")
     public String queryUserById(@PathVariable("userId") String userId) {
         return service.queryUserById(userId);
+    }
+
+
+    @GetMapping(value = "/instance", produces = "application/json")
+    public List<ServiceInstance> showInfo() {
+        return discoveryClient.getInstances("ribbon-api");
     }
 }
